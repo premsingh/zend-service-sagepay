e3d645987519887823182de5cd7db497a26af423
dcaunt <dcaunt@6748a79e-1f3c-11df-8328-bd766baa8347>
2010-06-24 09:13:15 +0000

    Added more constructor tests

--- tests/My/SagepayTest.php
+++ tests/My/SagepayTest.php
@@ -27,5 +27,38 @@ class My_SagepayTest extends PHPUnit_Framework_TestCase
             'vendor' => 'testVendor'
         ));
     }
-    
+
+    /**
+     * Test that a Zend_Config File can be used to set options
+     */
+    public function testSetOptionsWithZendConfig()
+    {
+        $params = array(
+            'currency' => 'GBP',
+            'vendor'   => 'myVendor',
+        );
+
+        $sagepay = new My_Sagepay(new Zend_Config($params));
+    }
+
+    /**
+     * Test log key sets the log
+     */
+    public function testLogOptionSetsLog()
+    {
+        $params = array(
+            'currency' => 'GBP',
+            'vendor'   => 'myVendor',
+            'log'      => new Zend_Log(new Zend_Log_Writer_Firebug()),
+        );
+        $sagepay = new My_Sagepay($params);
+
+        $log = $sagepay->getLog();
+
+        $this->assertThat(
+            $log,
+            $this->isInstanceOf('Zend_Log')
+        );
+    }
+
 }
