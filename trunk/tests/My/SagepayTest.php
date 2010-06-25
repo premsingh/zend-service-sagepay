858c06ff23676376d2b159cff33b01242cdc18a6
dcaunt <dcaunt@6748a79e-1f3c-11df-8328-bd766baa8347>
2010-06-25 13:45:15 +0000

    Added more tests for getters and setters

--- trunk/tests/My/SagepayTest.php
+++ trunk/tests/My/SagepayTest.php
@@ -4,7 +4,9 @@ class My_SagepayTest extends PHPUnit_Framework_TestCase
 {
     public function setUp()
     {
-        
+        $this->_sagepay = new My_Sagepay(array(
+            'vendor'    => 'myVendor'
+        ));
     }
 
     /**
@@ -28,6 +30,8 @@ class My_SagepayTest extends PHPUnit_Framework_TestCase
         );
 
         $sagepay = new My_Sagepay(new Zend_Config($params));
+        $this->assertEquals('GBP', $sagepay->getCurrency());
+        $this->assertEquals('myVendor', $sagepay->getVendor());
     }
 
     /**
@@ -48,6 +52,86 @@ class My_SagepayTest extends PHPUnit_Framework_TestCase
             $log,
             $this->isInstanceOf('Zend_Log')
         );
+    }
+
+    /**
+     * Test set currency
+     */
+    public function testSetCurrency()
+    {
+        $this->_sagepay->setCurrency('EUR');
+        $this->assertEquals('EUR', $this->_sagepay->getCurrency());
+    }
+
+    /**
+     * Test set vps protocol
+     */
+    public function testSetVpsProtocol()
+    {
+        $this->_sagepay->setVpsProtocol('2.52');
+        $this->assertEquals('2.52', $this->_sagepay->getVpsProtocol());
+    }
+
+    /**
+     * Test set test mode
+     */
+    public function testSetTestMode()
+    {
+        $this->_sagepay->setTestMode(true);
+        $this->assertEquals(true, $this->_sagepay->isTestMode());
+    }
+
+    /**
+     * Test set simulator mode
+     */
+    public function testSetSimulator()
+    {
+        $this->_sagepay->setSimulator(true);
+        $this->assertEquals(true, $this->_sagepay->isSimulator());
+    }
+
+    /**
+     * Test set vendor
+     */
+    public function testSetVendor()
+    {
+        $this->_sagepay->setVendor('superVendor');
+        $this->assertEquals('superVendor', $this->_sagepay->getVendor());
+    }
+    
+    /**
+     * Test that we always have a HTTP client, a default if none provided
+     */
+    public function testGetHttpClientAlwaysReturnsAClient()
+    {
+        $client = $this->_sagepay->getHttpClient();
+        
+        $this->assertThat(
+            $client,
+            $this->isInstanceOf('Zend_Http_Client')
+        );
+    }
+
+    /**
+     * Test that we always have a logger, even if it is the null object
+     */
+    public function testGetLogAlwaysReturnsALog()
+    {
+        $client = $this->_sagepay->getLog();
+
+        $this->assertThat(
+            $client,
+            $this->isInstanceOf('Zend_Log')
+        );
+    }
+
+    /**
+     * Test that simulator and test mode are not enabled by default
+     */
+    public function testSimulatorAndTestModeAreOffByDefault()
+    {
+        $this->assertFalse($this->_sagepay->isSimulator());
+        $this->assertFalse($this->_sagepay->isTestMode());
     }
 
 }
